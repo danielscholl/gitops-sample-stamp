136d23af06eb39826a143613e2074f8bebe65384
Daniel Scholl <dascholl@microsoft.com>
2022-12-12 00:26:06 +0000

    Validate updated

--- _scripts/validate.sh
+++ _scripts/validate.sh
@@ -9,11 +9,12 @@ echo "INFO - Downloading Flux OpenAPI schemas"
 mkdir -p /tmp/flux-crd-schemas/master-standalone-strict
 curl -sL https://github.com/fluxcd/flux2/releases/latest/download/crd-schemas.tar.gz | tar zxf - -C /tmp/flux-crd-schemas/master-standalone-strict
 
-find . -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
+find $PARENT_DIR -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
   do
     echo "INFO - Validating $file"
     yq e 'true' "$file" > /dev/null
 done
+
 
 kubeconform_config=( "-strict" "-ignore-missing-schemas" "-schema-location" "default" "-schema-location" "/tmp/flux-crd-schemas" "-verbose")
 
@@ -26,12 +27,13 @@ find $PARENT_DIR/clusters -maxdepth 2 -type f -name '*.yaml' -print0 | while IFS
     fi
 done
 
+
 # mirror kustomize-controller build options
 kustomize_flags=("--load-restrictor=LoadRestrictionsNone")
 kustomize_config="kustomization.yaml"
 
 echo "INFO - Validating kustomize overlays"
-find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
+find $PARENT_DIR -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
   do
     echo "INFO - Validating kustomization ${file/%$kustomize_config}"
     kustomize build "${file/%$kustomize_config}" "${kustomize_flags[@]}" | \
